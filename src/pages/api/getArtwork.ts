f2e0823973304d7b15c0a5f1bd935b3a67bc934c
Ian Sears <iansears11@gmail.com>
2024-02-28 18:57:00 -0800

    fix bad date hardcode

--- src/pages/api/getArtwork.ts
+++ src/pages/api/getArtwork.ts
@@ -22,24 +22,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const adjustedDate = new Date(currentDate.getTime() - timeZoneOffset);
   // const dateFolderName = adjustedDate.toISOString().split('T')[0];
   // Temporary hardcoding due to DALL-E billing limit
-  const dateFolderName = "2024-02-24";
+  const dateFolderName = "2024-02-25";
   const basePath = `${process.env.VERCEL_BLOB_STORE_NAME}/art/${dateFolderName}`;
-  // console.log(`basePath: ${basePath}`);
   const artworks: ArtworkDetails[] = [];
   const vercelBlobBaseUrl = process.env.VERCEL_BLOB_BASE_URL;
   try {
     // Assuming you have 9 folders for each day
     for (let i = 1; i <= 9; i++) {
       const artworkPath = `${basePath}/${i}`;
-      // console.log(`artworkPath for iteration ${i}: ${artworkPath}`);
       // Listing the contents of each artwork folder
       const files = await list({ bucketName: process.env.VERCEL_BLOB_STORE_NAME, prefix: artworkPath });
-      // console.log(`Files fetched for ${artworkPath}:`, files);
+      console.log(files);
       // Find image and artist files based on the naming convention and use their URLs directly
       const imageFile = files.blobs.find(file => file.url.includes('image_'));
       const artistFile = files.blobs.find(file => file.url.includes('artist_'));
-      // console.log(`imageFile:`, imageFile);
-      // console.log(`artistFile:`, artistFile);
+      console.log(`imageFile:`, imageFile);
+      console.log(`artistFile:`, artistFile);
       // Construct URLs for the artwork and artist images using the base URL
       const imageUrl = imageFile.url;
       const artistUrl = artistFile.url;
